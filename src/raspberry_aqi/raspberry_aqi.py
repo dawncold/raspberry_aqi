f43da094171656aaafcf2e35d642c180cc2f5cf1
TianZhen <loooseleaves@gmail.com>
2018-03-04 22:58:26 +0800

    fix: pms7003 return data

--- src/raspberry_aqi/raspberry_aqi.py
+++ src/raspberry_aqi/raspberry_aqi.py
@@ -39,18 +39,18 @@ if __name__ == '__main__':
             "city": config['city']
         },
         "fields": {
-            "pm1.0@cf1": data['data'][pms7003.P_CF_PM10],
-            "pm2.5@cf1": data['data'][pms7003.P_CF_PM25],
-            "pm10@cf1": data['data'][pms7003.P_CF_PM100],
-            "pm1.0": data['data'][pms7003.P_C_PM10],
-            "pm2.5": data['data'][pms7003.P_C_PM25],
-            "pm10": data['data'][pms7003.P_C_PM100],
-            "dl0.3@0.1l": data['data'][pms7003.P_C_03],
-            "dl0.5@0.1l": data['data'][pms7003.P_C_05],
-            "dl1.0@0.1l": data['data'][pms7003.P_C_10],
-            "dl2.5@0.1l": data['data'][pms7003.P_C_25],
-            "dl5.0@0.1l": data['data'][pms7003.P_C_50],
-            "dl10@0.1l": data['data'][pms7003.P_C_100],
+            "pm1.0@cf1": data['data'][pms7003.P_CF_PM10][1],
+            "pm2.5@cf1": data['data'][pms7003.P_CF_PM25][1],
+            "pm10@cf1": data['data'][pms7003.P_CF_PM100][1],
+            "pm1.0": data['data'][pms7003.P_C_PM10][1],
+            "pm2.5": data['data'][pms7003.P_C_PM25][1],
+            "pm10": data['data'][pms7003.P_C_PM100][1],
+            "dl0.3@0.1l": data['data'][pms7003.P_C_03][1],
+            "dl0.5@0.1l": data['data'][pms7003.P_C_05][1],
+            "dl1.0@0.1l": data['data'][pms7003.P_C_10][1],
+            "dl2.5@0.1l": data['data'][pms7003.P_C_25][1],
+            "dl5.0@0.1l": data['data'][pms7003.P_C_50][1],
+            "dl10@0.1l": data['data'][pms7003.P_C_100][1],
         }
     }
     client = InfluxDBClient(database=config['database'])
